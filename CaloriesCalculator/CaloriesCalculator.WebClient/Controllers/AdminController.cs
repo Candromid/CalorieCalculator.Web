e0e65d3aab9334e3dd8d087fc3b34a52c66e4de9
Nurbek Nurdaulet <mainaccnurbek@mail.ru>
2023-06-16 00:16:19 +0500

    usless commits

---
+++ CaloriesCalculator/CaloriesCalculator.WebClient/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaloriesCalculator.WebClient.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Admin() 
+        {
+            return View();
+        }
+    }
+}
