2150ec044d73a745d7046988032bf860c3d7a36d
Nurbek Nurdaulet <mainaccnurbek@mail.ru>
2023-06-20 23:44:16 +0500

    added appsettings and first steps for EF

---
+++ CaloriesCalculator/CaloriesCalculator.WebClient/Domain/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CaloriesCalculator.WebClient.Domain.Entities;
+using CaloriesCalculator.WebClient.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaloriesCalculator.WebClient.Domain
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Products> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.ConnectionString);
+        }
+    }
+}
---
+++ CaloriesCalculator/CaloriesCalculator.WebClient/Service/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace CaloriesCalculator.WebClient.Service
+{
+    public class Config
+    {
+        public static string ConnectionString { get; set; }
+    }
+}
