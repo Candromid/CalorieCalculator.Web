8223ca63a02e1193f39ba7160ffef3e3d04c5976
Nurbek Nurdaulet <mainaccnurbek@mail.ru>
2023-07-04 12:36:26 +0500

    End-transfering-admin-panel

---
+++ CaloriesCalculator/CaloriesCalculator.WebAdmin/Domain/DatabaseContext.cs
@@ -0,0 +1,39 @@
+﻿using CaloriesCalculator.WebAdmin.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaloriesCalculator.WebAdmin.Domain
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>(ConfigureProducts);
+        }
+
+        private void ConfigureProducts(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+            .IsRequired()
+             .HasMaxLength(100);
+
+            builder.Property(x => x.Proteins);
+
+            builder.Property(x => x.Fats);
+
+            builder.Property(x => x.Carbohydrates);
+
+        }
+
+    }
+}
---
+++ CaloriesCalculator/CaloriesCalculator.WebAdmin/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CaloriesCalculator.WebAdmin.Models
+{
+    public class ProductViewModel
+    {
+        public string? Name { get; set; }
+        public decimal Proteins { get; set; }
+        public decimal Fats { get; set; }
+        public decimal Carbohydrates { get; set; }
+    }
+}
