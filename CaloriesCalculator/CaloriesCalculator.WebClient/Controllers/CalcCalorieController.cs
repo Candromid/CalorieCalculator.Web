6dd08814ccb0eaa078a0a160d8a975ed1e423bf4
Nurbek Nurdaulet <mainaccnurbek@mail.ru>
2023-07-09 20:05:49 +0500

    Ended-create-page-with-calc

--- CaloriesCalculator/CaloriesCalculator.WebClient/Controllers/CalcCalorieController.cs
+++ CaloriesCalculator/CaloriesCalculator.WebClient/Controllers/CalcCalorieController.cs
@@ -1,53 +1,12 @@
-﻿using CaloriesCalculator.WebClient.Domain;
-using CaloriesCalculator.WebClient.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CaloriesCalculator.WebClient.Controllers
 {
     public class CalcCalorieController : Controller
     {
-        private readonly DatabaseContext _context;
-
-        public CalcCalorieController(DatabaseContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        [HttpGet]
-        public IActionResult SearchProducts(string query)
-        {
-            var products = _context.Products
-                .Where(p => p.Name.Contains(query))
-                .Select(p => new Product
-                {
-                    Name = p.Name
-                })
-                .ToList();
-
-            return Json(products);
-        }
-
-        [HttpPost]
-        public IActionResult CalculateCalories(string product)
-        {
-            var selectedProduct = _context.Products.FirstOrDefault(p => p.Name == product);
-
-            if (selectedProduct == null)
-            {
-                return BadRequest("Продукт не найден.");
-            }
-
-            // Расчет калорий на основе выбранного продукта и веса 100 грамм
-            decimal calories = (selectedProduct.Proteins + selectedProduct.Fats + selectedProduct.Carbohydrates) / 100;
-
-            return Json(new { calories });
         }
     }
 }
--- CaloriesCalculator/CaloriesCalculator.WebClient/Views/CalcCalorie/Index.cshtml.cs
+++ CaloriesCalculator/CaloriesCalculator.WebClient/Views/CalcCalorie/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CaloriesCalculator.WebClient.Views.CaloriesCalc
+namespace CaloriesCalculator.WebClient.Views.CalcCalorie
 {
     public class IndexModel : PageModel
     {
